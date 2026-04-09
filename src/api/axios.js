5acbbb98c0899badb16b14f38a304ef1c627e268
Eric Cossio <cossiovargase@gmail.com>
2026-04-09 10:56:41 -0600

    url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://icard-backend.onrender.com/api',
 })
 
 // Agrega el token JWT a cada request automáticamente
